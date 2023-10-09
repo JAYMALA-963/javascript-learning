a48dd06adb1dc34c509e589f588ef999450245a0
Jaymala <jiya36999@gmail.com>
2023-10-09 20:14:55 +0530

    data types

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,3 +3,20 @@ const accountId = 389283;
 let accountEmail = 'yorlin9@gmail.com';
 var accountPassword = '8349';
 accountCity = 'Dublin';
+
+
+/* data types 
+
+number => 2 to pawer 53
+bigint
+string => ""
+boolean => true / false
+null => standalone value
+undefined
+symbol => unique
+object 
+
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
